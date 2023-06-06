df34202db0a94a5cbdb206ad548ea70b7edb5571
Adam Chovanec <chovanec@tutanota.com>
2023-06-06 15:23:48 +0200

    fix: use XDG_RUNTIME_DIR or fallback to default

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,16 @@ struct Control {
 
 fn main() -> Result<()> {
     let args = Control::from_args();
-    let tmp = std::env::var("TMPDIR").unwrap_or("/tmp".to_owned());
 
-    match std::fs::read_dir(tmp) {
+    let runtime_dir = std::env::var("XDG_RUNTIME_DIR")
+        .unwrap_or("/tmp".to_string())
+        .to_owned();
+
+    match std::fs::read_dir(runtime_dir) {
         Ok(dir) => dir
             .filter_map(|f| f.ok())
-            .filter(|f| {
-                let is_dir =
-                    matches!(f.file_type().map(|t| t.is_dir()), Ok(true));
-                let name_heuristic =
-                    f.file_name().to_string_lossy().starts_with("nvim");
-                is_dir && name_heuristic
-            })
-            .filter_map(|dir| {
-                Some(
-                    std::fs::read_dir(dir.path())
-                        .ok()?
-                        .filter_map(Result::ok)
-                        .map(|d| d.path()),
-                )
-            })
-            .flatten()
-            .filter_map(|d| Session::new_unix_socket(d).ok())
+            .filter(|f| f.file_name().to_string_lossy().starts_with("nvim"))
+            .filter_map(|f| Session::new_unix_socket(f.path()).ok())
             .map(|mut session| {
                 session.start_event_loop();
                 Neovim::new(session)
